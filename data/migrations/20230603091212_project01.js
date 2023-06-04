3fa945dbef96270149fe5c0296e5a5d9bfbbea5c
bahasaracoglu <bahasaracoglu@gmail.com>
2023-06-04 14:36:01 +0300

    migrations / table schema created

--- data/migrations/20230603091212_project01.js
+++ data/migrations/20230603091212_project01.js
@@ -2,14 +2,64 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username").notNullable().unique();
+      users.string("password").notNullable();
+      users.string("emal").notNullable().unique();
+      users
+        .string("avatar_url")
+        .defaultTo(
+          "https://i.pinimg.com/564x/b9/68/3d/b9683d3fe3f25bca278364f64f215c2a.jpg"
+        );
+    })
+
+    .createTable("posts", (posts) => {
+      posts.increments("post_id");
+      posts.string("body", 280);
+      posts.timestamp("created_at").defaultTo(knex.fn.now());
+      posts.string("image_url");
+      posts
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+    })
+
+    .createTable("favorites", (favorites) => {
+      favorites.increments("favorite_id");
+      favorites
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+      favorites
+        .integer("post_id")
+        .unsigned()
+        .notNullable()
+        .references("post_id")
+        .inTable("posts")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  knex.schema
+    .dropTableIfExists("favorites")
+    .dropTableIfExists("posts")
+    .dropTableIfExists("users");
 };
--- data/seeds/project01.js
+++ data/seeds/project01.js
@@ -1,13 +1,21 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("users").truncate();
+  await knex("posts").truncate();
+  await knex("favorites").truncate();
+  await knex("users").insert([
+    {
+      username: "elonmusk",
+      password: "$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq", // password "1234"
+      email: "elonmusk@notmail.com",
+      avatar_url:
+        "https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg",
+    },
+    { id: 2, colName: "rowValue2" },
+    { id: 3, colName: "rowValue3" },
   ]);
 };
